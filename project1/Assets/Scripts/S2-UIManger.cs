4594f280551e5fc3a6c98b94a123258d34a02ab1
Ana María Ardila Pacheco <anaardila@uninorte.edu.co>
2025-05-18 23:46:40 -0500

    add touch rotator

--- project1/Assets/Scripts/S2-UIManger.cs
+++ project1/Assets/Scripts/S2-UIManger.cs
@@ -91,18 +91,6 @@ public class S2UIManager : MonoBehaviour
         ResetAllCanvases();
         ARPositionCanvas.SetActive(true);
 
-        //mainPPL.transform.GetChild(0).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
-        //mainPPL.transform.GetChild(1).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
-        //mainPPL.transform.GetChild(2).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
-
-        //mainMenuCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
-        //mainMenuCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
-
-
-        //itemsMenuCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
-        //itemsMenuCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
-        //itemsMenuCanvas.transform.GetChild(1).transform.DOMoveY(180, 0.3f);
-
         ARPositionCanvas.transform.GetChild(0).transform.DOScale(new Vector3(1, 1, 1), 0.3f);
         ARPositionCanvas.transform.GetChild(1).transform.DOScale(new Vector3(1, 1, 1), 0.3f);
         
